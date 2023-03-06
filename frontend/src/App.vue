253ffc830eb49baec5127eda4490ec5368f89b2b
neha2003 <neha.sq8@gmail.com>
2023-03-05 22:50:08 -0600

    deprecated python frontend and added vue frontend with tauri

---
+++ frontend/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <MainScreen />
+</template>
+
+<script>
+import MainScreen from './components/MainScreen.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MainScreen
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
