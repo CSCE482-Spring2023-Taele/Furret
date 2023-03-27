fa2fb162795d8a5168b305dc5668a985af721f18
Akash Jothi <56031688+akashyftw@users.noreply.github.com>
2023-03-26 23:05:30 -0500

    Attempted to fix audio to midi problems

--- Server/backend/audio_to_midi.py
+++ Server/backend/audio_to_midi.py
@@ -22,3 +22,17 @@ for pitch in pitches:
         midi_note = int(round(69 + 12 * math.log2(np.argmax(pitch) / 440.0)))
     midi_notes.append(midi_note)
 
+# Create a MIDI file using pretty_midi
+midi = pretty_midi.PrettyMIDI()
+piano = pretty_midi.Instrument(program=0)
+start_time = 0
+for note in midi_notes:
+    if note is None:
+        start_time += 0.01
+    else:
+        end_time = start_time + 0.01
+        piano.notes.append(pretty_midi.Note(
+            velocity=100,
+            pitch=note,
+            start=start_time,
+
