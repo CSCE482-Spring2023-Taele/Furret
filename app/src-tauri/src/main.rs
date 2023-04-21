769a5217565c788014919605e5ce64bd31a4018c
Thionazin <senhehao@senhehao.com>
2023-04-20 21:06:08 -0500

    partially implemented rust function to be called from frontend

--- app/src-tauri/src/main.rs
+++ app/src-tauri/src/main.rs
@@ -3,9 +3,16 @@
   windows_subsystem = "windows"
 )]
 
+#[tauri::command]
+fn test_command() {
+  println!("I was invoked from JS!");
+}
+
+
 fn main() {
   tauri::Builder::default()
     .plugin(tauri_plugin_sql::Builder::default().build())
+    .invoke_handler(tauri::generate_handler!(test_command))
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
