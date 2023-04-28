7f15e5a86704da5cc78ab032e840da8c84ef1b8a
Krishnan Prashanth <krishnan.prashanth@tamu.edu>
2023-04-27 22:16:28 -0500

    Midi Transcription Speed Enhanced

--- app/src-tauri/Analysis/backend/audio2Midi.py
+++ app/src-tauri/Analysis/backend/audio2Midi.py
@@ -1,36 +1,28 @@
-import librosa
-import numpy as np
-import mido
 import sys
-import librosa
-import crepe
-from mido import Message, MidiFile, MidiTrack
+import subprocess
+import codecs
+from basic_pitch.inference import predict
+from basic_pitch import ICASSP_2022_MODEL_PATH
+import os
+import chardet
 
 
 class Audio2Midi:
-    def __init__(self, audioFilePath, midiFilePath = "userInput.mid"):
-        self.audio_to_midi(audioFilePath, midiFilePath)
-    
-    def audio_to_midi(self, audio_file, midi_file):
-        # Load the audio file
-        audio_data, sr = librosa.load(audio_file, sr=44100, mono=True)
-
-        # Predict the pitch of the audio signal using Crepe
-        time, frequency, confidence, activation = crepe.predict(audio_data, sr=sr, viterbi=True)
-
-        # Convert the pitch predictions to MIDI notes
-        midi_notes = []
-        for t, f in zip(time, frequency):
-            midi_note = int(round(librosa.hz_to_midi(f)))
-            midi_notes.append((t, midi_note))
-
-        # Save the MIDI notes to a file
-        with open(midi_file, 'w') as f:
-            for t, note in midi_notes:
-                f.write('%.2f,%d\n' % (t, note))
+    def __init__(self, audioFilePath, midiFilePath = "./audioMidiTemp"):
+        subprocess.run(["basic-pitch", midiFilePath, audioFilePath, "--save-midi"])
+        path = "./audioMidiTemp"
+        fileName = os.listdir(path)[0]
+        # Detect the encoding of the file
+        with open(fileName, 'rb') as f:
+            result = chardet.detect(f.read())
+            
+        # Open the file in the detected encoding and write to a new UTF-8 file
+        with open(fileName, encoding=result['encoding']) as f, open('file_utf8.txt', 'w', encoding='utf-8') as f_utf8:
+            f_utf8.write(f.read())
+        
         
 
-
+    
 def main():
     Audio2Midi(sys.argv[1])
 
