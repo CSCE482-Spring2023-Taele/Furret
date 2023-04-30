684957ad77d923168ce1bb03ac8636d40ca8174c
Krishnan Prashanth <krishnan.prashanth@tamu.edu>
2023-04-29 19:34:56 -0500

    Testing on Criminal

--- app/src-tauri/Analysis/backend/audio2Midi.py
+++ app/src-tauri/Analysis/backend/audio2Midi.py
@@ -1,17 +1,13 @@
 import sys
 import subprocess
-import codecs
-from basic_pitch.inference import predict
-from basic_pitch import ICASSP_2022_MODEL_PATH
 import os
-import chardet
 
 
 class Audio2Midi:
-    def __init__(self, audioFilePath, midiFilePath = "./audioMidiTemp"):
-        subprocess.run(["basic-pitch", midiFilePath, audioFilePath, "--save-midi"])
-        path = "./audioMidiTemp"
-        fileName = os.listdir(path)[0]
+    def __init__(self, audioFilePath, midiFilePath = "userInput.mid"):
+        subprocess.run(["basic-pitch", ".", audioFilePath, "--save-midi"])
+        # path = "./audioMidiTemp"
+        # fileName = os.listdir(path)[0]
         # # Detect the encoding of the file
         # with open(fileName, 'rb') as f:
         #     result = chardet.detect(f.read())
